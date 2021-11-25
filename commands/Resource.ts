070e91282e91ec08180a69d19210749de1b08142
caiodomingues <caiodss99@hotmail.com>
2021-11-25 00:51:09 -0300

    feat: add Resource command

--- commands/Resource.ts
+++ commands/Resource.ts
@@ -1,16 +1,16 @@
-import { BaseCommand, args } from "@adonisjs/core/build/standalone";
-import { join } from "path";
+import { BaseCommand, args } from '@adonisjs/core/build/standalone'
+import { join } from 'path'
 
 export default class Resource extends BaseCommand {
   /**
    * Command name is used to run the command
    */
-  public static commandName = "make:resource";
+  public static commandName = 'make:resource'
 
   /**
    * Command description is displayed in the "help" output
    */
-  public static description = "Make a new Resource file";
+  public static description = 'Make a new Resource file'
 
   public static settings = {
     /**
@@ -24,36 +24,36 @@ export default class Resource extends BaseCommand {
      * you manually decide to exit the process
      */
     stayAlive: false,
-  };
+  }
 
-  @args.string({ description: "Resource name", required: true })
-  public name: string;
+  @args.string({ description: 'Resource name', required: true })
+  public name: string
 
   private nameFormatter = (name: string) => {
-    return name.includes("Resource") ? name : `${name}Resource`;
-  };
+    return name.includes('Resource') ? name : `${name}Resource`
+  }
 
   private modelFormatter = (model: string) => {
-    return model.includes("Resource") ? model.replace("Resource", "") : model;
-  };
+    return model.includes('Resource') ? model.replace('Resource', '') : model
+  }
 
   public async run() {
     this.generator
       .addFile(this.name, {
-        form: "singular",
-        extname: ".ts",
-        pattern: "pascalcase",
-        suffix: "Resource",
+        form: 'singular',
+        extname: '.ts',
+        pattern: 'pascalcase',
+        suffix: 'Resource',
       })
       .appRoot(this.application.appRoot)
-      .destinationDir("app/Resources")
+      .destinationDir('app/Resources')
       .useMustache()
-      .stub(join(__dirname, "./templates/resource.txt"))
+      .stub(join(__dirname, './templates/resource.txt'))
       .apply({
         name: this.nameFormatter(this.name),
         model: this.modelFormatter(this.name),
-      });
+      })
 
-    await this.generator.run();
+    await this.generator.run()
   }
 }
